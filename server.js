095af133c28f3747115ce4f29b633d0440f3c6ab
hunter-aw <hunteraweinsheink@gmail.com>
2018-10-17 15:41:13 +0300

    added basic login

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser')
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
 const app = express();
+
 
 // Parsers for POST data
 app.use(bodyParser.json());
@@ -14,11 +16,6 @@ app.use(passport.initialize());
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'src')));
 
-// catch all other routes and return the index file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/index.html'))
-});
-
 const port = process.env.PORT || '8000';
 app.set('port', port);
 
@@ -26,5 +23,24 @@ const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/Login.html'));
+    res.sendFile(path.join(__dirname, 'src/login.html'));
   });
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    if ((username === "john") && (password === "password")) {
+        return done(null, { username: username, id: 1 });
+    } else {
+        return done(null, false);
+    }
+}));
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login?err',
+    session: false
+}));
+
+// catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/index.html'))
+});
